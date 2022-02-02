aeadda7593a20eda610ddaf4a7d056b8a27888fd
AdiCotirla <adioprea53@gmail.com>
2022-02-02 19:46:08 +0200

    nr telefon

--- src/Data/DriverData.js
+++ src/Data/DriverData.js
@@ -5,7 +5,7 @@ const driverData = [
         name: "Marcel",
         profilImage: profil,
         infos: "Foarte responsabil si respectos. Ajunge mereu la tine in cea mai scurta perioada de timp si iti ofere cele mai bune servicii din oras.",
-        contact: "tel:+0751 634 462"
+        contact: "tel: 0751 634 462"
     }
 ]
 
